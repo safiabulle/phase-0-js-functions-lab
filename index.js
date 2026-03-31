38063025c22ece2e5a2b542b0481bb0c1f6b2346
Safia Bulle <safia.bulle@student.moringaschool.com>
2026-03-31 10:48:33 +0300

    added functions to index.js file

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+function calculateTax(amount){
+   return amount * 0.10;
+}
 
+function convertToUpperCase(text){
+    return text.toUpperCase();
 
+}
 
+function findMaximum(num1 , num2){
+    if (num1 > num2){
+        return num1;
+        }else{
+            return num2;
+        }
+}
+
+function isPalindrome(word){
+    const cleanWord = word.toLowerCase();
+    const reverseWord = cleanWord.split('').reverse().join('');
+    return cleanWord === reverseWord;
+}
+function calculateDiscountedPrice(originalPrice,discountPercentage){
+   const discountAmount = originalPrice * (discountPercentage / 100);
+    return originalPrice-discountAmount;
+}
 
 
 // This is required for the test to function properly  
